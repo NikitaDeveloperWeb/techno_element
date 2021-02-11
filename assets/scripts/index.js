bae2fa8aaa86ae87eea74dcc31c273b38e921551
Nikita <rusakdeveloepr@gmail.com>
2021-02-11 18:52:32 +0300

    deploy

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -1,71 +1,8 @@
 // class for product
-class Products {
-  constructor(selector, data, state) {
-    // selector
-    this.selector = selector;
-    // element
-    this.$el = $(selector);
-    this.$block = $(selector).find('.products__block');
-    this.$table = $(selector).find('.products__table');
-    this.$description = false;
-    // data
-    this.data = data;
-    // state
-    this.state = state;
-
-    // methods
-
-    this.render();
-    this.setup();
-  }
-  render() {
-    let list = this.data.map((prod, index) => {
-      return `
-      <div class="product" id='${index}prod'>
-          <div class="product__number">${prod.number}</div>
-          <div div class="product__img">
-            <img src=${prod.image}/>
-          </div>
-          <div class="product__title" data-type='input'>${prod.title}</div>
-          ${
-            this.$description
-              ? `<div class="product__description">${prod.description}</div>`
-              : `<div class="product__description">${prod.description.substr(0, 49)}...</div>`
-          }
-          <div class="product__action">
-            ${
-              prod.price != ''
-                ? `<div class="product__action__price">${prod.price} руб.</div><button>В корзину</button>`
-                : `<button class="default">Запросить цену</button>`
-            }
-          </div>
-
-    </div>`;
-    });
-    this.state == 'block' ? this.$block.html(list) : this.$table.html(list);
-  }
-  setup() {
-    this.clickHandler = this.clickHandler.bind(this);
-    this.$el.addEventListener('click', this.clickHandler);
-  }
-  openDescript() {}
-  clickHandler(event) {
-    // const { type } = event.target.dataset;
-    // if (type === 'input') {
-    //   document.onclick = (e) => {};ё
-    // }
-  }
-
-  //delete listener
-  destroy() {
-    this.$el.removeEventListener('click', this.clickHandler);
-  }
-}
-// элементы массива
 const data = [
   {
     number: 'F10023-7D',
-    title: 'Болгарка Skil 900s LA',
+    title: 'Болгарка Skil 900s LA1',
     image:
       'https://cdnmedia.220-volt.ru/content/products/498/498581/images/original/n1200x800_q80/1.jpeg',
     description:
@@ -74,7 +11,7 @@ const data = [
   },
   {
     number: 'F10023-7D',
-    title: 'Болгарка Skil 900s LA',
+    title: 'Болгарка Skil 900s LA2',
     image:
       'https://cdnmedia.220-volt.ru/content/products/498/498581/images/original/n1200x800_q80/1.jpeg',
     description:
@@ -83,7 +20,7 @@ const data = [
   },
   {
     number: 'F10023-7D',
-    title: 'Болгарка Skil 900s LA',
+    title: 'Болгарка Skil 900s LA3',
     image:
       'https://cdnmedia.220-volt.ru/content/products/498/498581/images/original/n1200x800_q80/1.jpeg',
     description:
@@ -92,7 +29,7 @@ const data = [
   },
   {
     number: 'F10023-7D',
-    title: 'Болгарка Skil 900s LA',
+    title: 'Болгарка Skil 900s LA4',
     image:
       'https://cdnmedia.220-volt.ru/content/products/498/498581/images/original/n1200x800_q80/1.jpeg',
     description:
@@ -101,7 +38,7 @@ const data = [
   },
   {
     number: 'F10023-7D',
-    title: 'Болгарка Skil 900s LA',
+    title: 'Болгарка Skil 900s LA5',
     image:
       'https://cdnmedia.220-volt.ru/content/products/498/498581/images/original/n1200x800_q80/1.jpeg',
     description:
@@ -110,7 +47,7 @@ const data = [
   },
   {
     number: 'F10023-7D',
-    title: 'Болгарка Skil 900s LA',
+    title: 'Болгарка Skil 900s LA6',
     image:
       'https://cdnmedia.220-volt.ru/content/products/498/498581/images/original/n1200x800_q80/1.jpeg',
     description:
@@ -119,7 +56,7 @@ const data = [
   },
   {
     number: 'F10023-7D',
-    title: 'Болгарка Skil 900s LA',
+    title: 'Болгарка Skil 900s LA7',
     image:
       'https://cdnmedia.220-volt.ru/content/products/498/498581/images/original/n1200x800_q80/1.jpeg',
     description:
@@ -128,7 +65,7 @@ const data = [
   },
   {
     number: 'F10023-7D',
-    title: 'Болгарка Skil 900s LA',
+    title: 'Болгарка Skil 900s LA8',
     image:
       'https://cdnmedia.220-volt.ru/content/products/498/498581/images/original/n1200x800_q80/1.jpeg',
     description:
@@ -137,7 +74,7 @@ const data = [
   },
   {
     number: 'F10023-7D',
-    title: 'Болгарка Skil 900s LA',
+    title: 'Болгарка Skil 900s LA9',
     image:
       'https://cdnmedia.220-volt.ru/content/products/498/498581/images/original/n1200x800_q80/1.jpeg',
     description:
@@ -146,7 +83,7 @@ const data = [
   },
   {
     number: 'F10023-7D',
-    title: 'Болгарка Skil 900s LA',
+    title: 'Болгарка Skil 900s LA10',
     image:
       'https://cdnmedia.220-volt.ru/content/products/498/498581/images/original/n1200x800_q80/1.jpeg',
     description:
@@ -155,7 +92,7 @@ const data = [
   },
   {
     number: 'F10023-7D',
-    title: 'Болгарка Skil 900s LA',
+    title: 'Болгарка Skil 900s LA11',
     image:
       'https://cdnmedia.220-volt.ru/content/products/498/498581/images/original/n1200x800_q80/1.jpeg',
     description:
@@ -164,7 +101,7 @@ const data = [
   },
   {
     number: 'F10023-7D',
-    title: 'Болгарка Skil 900s LA',
+    title: 'Болгарка Skil 900s LA12',
     image:
       'https://cdnmedia.220-volt.ru/content/products/498/498581/images/original/n1200x800_q80/1.jpeg',
     description:
@@ -173,7 +110,7 @@ const data = [
   },
   {
     number: 'F10023-7D',
-    title: 'Болгарка Skil 900s LA',
+    title: 'Болгарка Skil 900s LA13',
     image:
       'https://cdnmedia.220-volt.ru/content/products/498/498581/images/original/n1200x800_q80/1.jpeg',
     description:
@@ -182,7 +119,7 @@ const data = [
   },
   {
     number: 'F10023-7D',
-    title: 'Болгарка Skil 900s LA',
+    title: 'Болгарка Skil 900s LA14',
     image:
       'https://cdnmedia.220-volt.ru/content/products/498/498581/images/original/n1200x800_q80/1.jpeg',
     description:
@@ -191,7 +128,7 @@ const data = [
   },
   {
     number: 'F10023-7D',
-    title: 'Болгарка Skil 900s LA',
+    title: 'Болгарка Skil 900s LA15',
     image:
       'https://cdnmedia.220-volt.ru/content/products/498/498581/images/original/n1200x800_q80/1.jpeg',
     description:
@@ -200,7 +137,7 @@ const data = [
   },
   {
     number: 'F10023-7D',
-    title: 'Болгарка Skil 900s LA',
+    title: 'Болгарка Skil 900s LA16',
     image:
       'https://cdnmedia.220-volt.ru/content/products/498/498581/images/original/n1200x800_q80/1.jpeg',
     description:
@@ -208,5 +145,252 @@ const data = [
     price: '327 000',
   },
 ];
-// экземпляр
-const Bulgarian = new Products('.products', data, 'block');
+let typePreview = false;
+let $listProd = $('#products');
+class Product {
+  constructor(selector, data) {
+    // elem
+    this.$parent = parent;
+    this.selector = selector;
+    this.$component = $(selector);
+    this.$block = $('#products').find('.products__block');
+    this.$table = $('#products').find('.products__table');
+    this.html = ``;
+    // data
+    this.data = data;
+    // state
+    this.descriptionState = false;
+    this.titleState = false;
+    this.appState = false;
+
+    // methods
+    this.render(this.html);
+    this.setup();
+  }
+  render(html) {
+    if (!this.appState) {
+      html = this.data.map((prod, index) => {
+        return `<li class="product tasks__item" id=${index} draggable="true">
+      <div class="product__number">${prod.number}</div>
+      <div div class="product__img">
+        <img src=${prod.image}/>
+      </div>
+      ${
+        this.titleState
+          ? `<form class="form" data-type='title'>
+      <input type="text">
+      <input type="submit" value="Готово">
+      </form>`
+          : `<div class="product__title" data-type='title'>${prod.title}</div>`
+      }
+      ${
+        this.descriptionState
+          ? `<div class="product__description" data-type="description__full">${prod.description}</div>`
+          : `<div class="product__description" data-type="description__min">${prod.description.substr(
+              0,
+              49,
+            )}...</div>`
+      }
+      <div class="product__action">
+        ${
+          prod.price != ''
+            ? `<div class="product__action__price">${prod.price} руб.</div><button>В корзину</button>`
+            : `<button class="default">Запросить цену</button>`
+        }
+      </div>
+  </li>`;
+      });
+    } else {
+      html = ``;
+    }
+    !this.appState ? this.$block.html(html) : this.$table.html(html);
+  }
+  openDescription() {
+    this.descriptionState = true;
+  }
+  closeDescription() {
+    this.descriptionState = false;
+  }
+  rename() {
+    this.titleState = !this.titleState;
+  }
+
+  clickHandler(event) {
+    const { type } = event.target.dataset;
+
+    if (type === 'description__min') {
+      this.openDescription();
+      this.render();
+    } else if (type === 'description__full') {
+      this.closeDescription();
+      this.render();
+    } else if (type === 'title') {
+      this.rename();
+      this.render();
+    }
+  }
+  mouseHandler() {
+    this.appState = !this.appState;
+    this.render(this.appState);
+  }
+  setup() {
+    this.clickHandler = this.clickHandler.bind(this);
+    this.$parent.addEventListener('click', this.clickHandler);
+    this.mouseHandler = this.mouseHandler.bind(this);
+    document
+      .querySelector('#switch__preview')
+      .addEventListener('mouseover', this.mouseHandler, false);
+  }
+
+  //delete listener
+  destroy() {
+    this.$component.removeEventListener('click', this.clickHandler);
+  }
+}
+// drag and drop
+const tasksListElement = document.querySelector(`.tasks__list`);
+const taskElements = tasksListElement.querySelectorAll(`.product`);
+for (const task of taskElements) {
+  task.draggable = true;
+}
+
+tasksListElement.addEventListener(`dragstart`, (evt) => {
+  evt.target.classList.add(`selected`);
+});
+
+tasksListElement.addEventListener(`dragend`, (evt) => {
+  evt.target.classList.remove(`selected`);
+});
+
+tasksListElement.addEventListener(`dragover`, (evt) => {
+  // Разрешаем сбрасывать элементы в эту область
+  evt.preventDefault();
+
+  // Находим перемещаемый элемент
+  const activeElement = tasksListElement.querySelector(`.selected`);
+  // Находим элемент, над которым в данный момент находится курсор
+  const currentElement = evt.target;
+  // Проверяем, что событие сработало:
+  // 1. не на том элементе, который мы перемещаем,
+  // 2. именно на элементе списка
+  const isMoveable =
+    activeElement !== currentElement && currentElement.classList.contains(`tasks__item`);
+
+  // Если нет, прерываем выполнение функции
+  if (!isMoveable) {
+    return;
+  }
+
+  // Находим элемент, перед которым будем вставлять
+  const nextElement =
+    currentElement === activeElement.nextElementSibling
+      ? currentElement.nextElementSibling
+      : currentElement;
+
+  // Вставляем activeElement перед nextElement
+  tasksListElement.insertBefore(activeElement, nextElement);
+});
+
+const getNextElement = (cursorPosition, currentElement) => {
+  // Получаем объект с размерами и координатами
+  const currentElementCoord = currentElement.getBoundingClientRect();
+  // Находим вертикальную координату центра текущего элемента
+  const currentElementCenter = currentElementCoord.y + currentElementCoord.height / 2;
+
+  // Если курсор выше центра элемента, возвращаем текущий элемент
+  // В ином случае — следующий DOM-элемент
+  const nextElement =
+    cursorPosition < currentElementCenter ? currentElement : currentElement.nextElementSibling;
+
+  return nextElement;
+};
+
+tasksListElement.addEventListener(`dragover`, (evt) => {
+  evt.preventDefault();
+
+  const activeElement = tasksListElement.querySelector(`.selected`);
+  const currentElement = evt.target;
+  const isMoveable =
+    activeElement !== currentElement && currentElement.classList.contains(`tasks__item`);
+
+  if (!isMoveable) {
+    return;
+  }
+
+  // evt.clientY — вертикальная координата курсора в момент,
+  // когда сработало событие
+  const nextElement = getNextElement(evt.clientY, currentElement);
+
+  // Проверяем, нужно ли менять элементы местами
+  if (
+    (nextElement && activeElement === nextElement.previousElementSibling) ||
+    activeElement === nextElement
+  ) {
+    // Если нет, выходим из функции, чтобы избежать лишних изменений в DOM
+    return;
+  }
+
+  tasksListElement.insertBefore(activeElement, nextElement);
+});
+// menu
+const handler = () => {
+  document.querySelector('.menu__first').classList.add('open');
+};
+const remove = () => {
+  document.querySelector('.menu__first').classList.remove('open');
+};
+
+const handler2 = () => {
+  document.querySelector('.menu__second').classList.add('open');
+};
+const remove2 = () => {
+  document.querySelector('.menu__second').classList.remove('open');
+};
+const Switch = document.querySelector('.menu__switch');
+const Switch_two = document.querySelector('.menu__switch__two');
+Switch.addEventListener('mouseover', handler, false);
+Switch.addEventListener('mouseleave', remove, false);
+Switch_two.addEventListener('mouseover', handler2, false);
+Switch_two.addEventListener('mouseleave', remove2, false);
+const Balgarian = new Product('.product', data, '#products');
+
+// scobss
+let brackets = {
+  '[': ']',
+  '{': '}',
+  '(': ')',
+  '<': '>',
+};
+
+function correctBrackets(str) {
+  const vals = Object.keys(brackets).map((key) => brackets[key]);
+  const keys = Object.keys(brackets);
+
+  if (vals.indexOf(str[0]) + 1) {
+    return null;
+  }
+
+  let mass = str.split('');
+  let stack = [];
+
+  for (let i = 0; i < mass.length; i++) {
+    const l = mass[i];
+
+    if (keys.indexOf(l) + 1) {
+      stack.push(l);
+    } else {
+      if (stack.length) {
+        mass[i] = brackets[stack.shift()];
+      } else {
+        return null;
+      }
+    }
+  }
+
+  if (stack.length) {
+    return null;
+  }
+
+  return mass.join('');
+}
+console.log(correctBrackets('(())'));
